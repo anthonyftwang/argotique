50ef84af85febc2e99a61ea520518c2c76458e13
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2021-11-19 17:03:11 -0500

    refactor: use tile loading animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Loading.css';
 
 export const Loading = () => (
-  <div className="loading">
-    <div />
-    <div />
-    <div />
-    <div />
+  <div className="loadingContainer">
+    <div className="loading">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 )
