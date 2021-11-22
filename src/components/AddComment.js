a391a3f94fc88aa707740e4679876dc6dd975b62
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2021-11-22 01:39:18 -0500

    feat: create comments on posts

---
+++ src/components/AddComment.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './AddComment.css';
+
+export const AddComment = (props) => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      comment: '',
+    },
+    onSubmit: values => {
+      props.onSubmitHandler(values.comment);
+      formik.resetForm();
+    },
+  });
+  return (
+    <div className="addComment">
+      <div className="accordian">
+        <Accordion elevation={0}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="expand-comment-form"
+            sx={{
+              "&.Mui-expanded": {
+                minHeight: 0
+              },
+              "& .MuiAccordionSummary-content.Mui-expanded": {
+                margin: "12px 0"
+              }
+            }}
+          >
+            <h2 style={{margin:0}}>Comments</h2>
+          </AccordionSummary>
+          <AccordionDetails>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="commentForm">
+                <AddCommentIcon className="commentIcon" color="action" />
+                <div className="commentFieldContainer">
+                  <Input
+                    className="commentField"
+                    name="comment"
+                    placeholder="Write a comment..."
+                    fullWidth
+                    value={formik.values.comment}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+                <Button
+                  className="commentSubmit"
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                  disabled={!(formik.isValid && formik.dirty)}>
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -3,17 +3,25 @@ import { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
 import Auth from '@aws-amplify/auth';
 import { getPost, listComments } from '../graphql/queries';
-import { updatePost, deletePost, createComment } from '../graphql/mutations';
+import { updatePost, deletePost, createComment as createCommentMutation } from '../graphql/mutations';
 import { useLocation } from 'react-router-dom';
 import moment from 'moment';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import { Post } from '../components/Post';
 import { Comment } from '../components/Comment';
+import { AddComment } from '../components/AddComment';
 import { Loading } from '../components/Loading';
 import './PostPage.css';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export const PostPage = () => {
   const [post, setPost] = useState();
   const [comments, setComments] = useState([]);
+  const [alertOpen, setAlertOpen] = useState();
   const location = useLocation();
 
   useEffect(() => {
@@ -46,39 +54,80 @@ export const PostPage = () => {
     }
   }
 
+  async function createComment(comment) {
+    let date = new Date();
+    let user = await Auth.currentAuthenticatedUser();
+    const params = {
+      content: comment,
+      createdAt: date.toISOString(),
+      updatedAt: date.toISOString(),
+      userID: user.attributes.sub,
+      postID: post.id
+    };
+    await API.graphql({
+      query: createCommentMutation,
+      variables: { input: params }
+    });
+    // also update comment count
+    const updatedPostParams = {
+      id: post.id,
+      commentCount: post.commentCount + 1
+    };
+    await API.graphql({
+      query: updatePost,
+      variables: {input: updatedPostParams }
+    });
+    fetchPostAndComments();
+    setAlertOpen(true);
+  }
+
+  const handleAlertClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setAlertOpen(false);
+  };
+
   return (
-    <div className="postPage">
+    <div className="postPageContainer">
       {post ? (
-        <Post
-          className="postDetailsCard"
-          isPreview={false}
-          id={post.id}
-          username={post.user.name}
-          title={post.title}
-          subtitle={post.subtitle}
-          content={post.content}
-          voteCount={post.voteCount}
-          commentCount={post.commentCount}
-          contentAge={moment(post.lastActivityAt).fromNow()}
-          isLiked={post.isLiked}
-          isOwnedByUser={post.isOwnedByUser}
-        />
+        <div className="postPage">
+          <Post
+            className="postDetailsCard"
+            isPreview={false}
+            id={post.id}
+            username={post.user.name}
+            title={post.title}
+            subtitle={post.subtitle}
+            content={post.content}
+            voteCount={post.voteCount}
+            commentCount={post.commentCount}
+            contentAge={moment(post.lastActivityAt).fromNow()}
+            isLiked={post.isLiked}
+            isOwnedByUser={post.isOwnedByUser}
+          />
+          <div className="commentsList">
+            <AddComment onSubmitHandler={createComment} />
+            {!!comments.length &&
+              comments.map(comment => (
+                <Comment
+                  className="postComment"
+                  content={comment.content}
+                  username={comment.user.name}
+                  contentAge={moment(comment.createdAt).fromNow()}
+                />
+              ))
+            }
+          </div>
+          <Snackbar open={alertOpen} autoHideDuration={6000} onClose={handleAlertClose}>
+            <Alert onClose={handleAlertClose} severity="success" sx={{ width: '100%' }}>
+              Comment submitted!
+            </Alert>
+          </Snackbar>
+        </div>
       ) : (
         <Loading />
       )}
-      {!!comments.length &&
-        <div className="commentsList">
-          <h2>Comments</h2>
-          {comments.map(comment => (
-            <Comment
-              className="postComment"
-              content={comment.content}
-              username={comment.user.name}
-              contentAge={moment(comment.createdAt).fromNow()}
-            />
-          ))}
-        </div>
-      }
     </div>
   )
 }
