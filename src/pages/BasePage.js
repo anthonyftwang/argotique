e2646851a0e2850067e3d3c10586f54deb69710e
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2021-11-25 23:03:45 -0500

    feat: two-column layout for posts and menu

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
+import './BasePage.css';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
