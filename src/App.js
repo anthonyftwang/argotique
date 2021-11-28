a5a41228b3c0b801284c54f37b9fa1e27a2eba46
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2021-11-28 16:17:29 -0500

    refactor: remove phone number auth requirement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from 'react-router-dom';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { Nav } from './components/Nav.js';
@@ -16,10 +17,26 @@ import { UserPage } from './pages/UserPage.js';
 import { BasePage } from './pages/BasePage.js';
 import './App.css';
 
-function App() {
+import Amplify from 'aws-amplify';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+export default function App() {
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
   const theme = useTheme();
   const wideScreen = !useMediaQuery(theme.breakpoints.down('md'));
-  return (
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData)
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <Router basename="/">
       <div>
         <Nav />
@@ -34,7 +51,16 @@ function App() {
         </main>
       </div>
     </Router>
-  )
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" }
+        ]}
+      />
+    </AmplifyAuthenticator>
+  );
 }
-
-export default withAuthenticator(App);
