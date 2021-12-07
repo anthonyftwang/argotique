d50b1dedd9074e2319d620d4ef13319e4f33ce78
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2021-12-07 12:46:47 -0500

    feat: integrate new amplify auth form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,8 +6,9 @@ import {
   Navigate,
 } from 'react-router-dom';
 import Amplify, { Auth } from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { Authenticator } from '@aws-amplify/ui-react';
+// eslint-disable-next-line import/no-unresolved
+import '@aws-amplify/ui-react/styles.css';
 import { useTheme, useMediaQuery } from '@mui/material';
 import Nav from 'components/Nav/Nav';
 import DrawerMenu from 'components/DrawerMenu/DrawerMenu';
@@ -23,68 +24,52 @@ Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 
 function App() {
-  const [authState, setAuthState] = useState();
-  const [user, setUser] = useState();
   const theme = useTheme();
   const wideScreen = !useMediaQuery(theme.breakpoints.down('md'));
 
-  useEffect(() => {
-    return onAuthUIStateChange((nextAuthState, authData) => {
-      setAuthState(nextAuthState);
-      setUser(authData);
-    });
-  }, []);
-
-  return authState === AuthState.SignedIn && user ? (
-    <Router basename="/">
-      <div>
-        <Nav />
-        <main>
-          <Routes>
-            <Route
-              path="/post/:id"
-              element={
-                <BasePage childView={<PostPage />} key={Math.random()} />
-              }
-            />
-            <Route
-              path="/user/:id"
-              element={
-                <BasePage childView={<UserPage />} key={Math.random()} />
-              }
-            />
-            <Route
-              path="/liked"
-              element={
-                <BasePage childView={<LikedPage />} key={Math.random()} />
-              }
-            />
-            <Route
-              path="/"
-              element={
-                <BasePage childView={<HomePage />} key={Math.random()} />
-              }
-            />
-            <Route
-              path="*"
-              element={<Navigate replace to="/" key={Math.random()} />}
-            />
-          </Routes>
-          {wideScreen && <DrawerMenu />}
-        </main>
-      </div>
-    </Router>
-  ) : (
-    <AmplifyAuthenticator>
-      <AmplifySignUp
-        slot="sign-up"
-        formFields={[
-          { type: 'username' },
-          { type: 'password' },
-          { type: 'email' },
-        ]}
-      />
-    </AmplifyAuthenticator>
+  return (
+    <Authenticator>
+      {() => (
+        <Router basename="/">
+          <div>
+            <Nav />
+            <main>
+              <Routes>
+                <Route
+                  path="/post/:id"
+                  element={
+                    <BasePage childView={<PostPage />} key={Math.random()} />
+                  }
+                />
+                <Route
+                  path="/user/:id"
+                  element={
+                    <BasePage childView={<UserPage />} key={Math.random()} />
+                  }
+                />
+                <Route
+                  path="/liked"
+                  element={
+                    <BasePage childView={<LikedPage />} key={Math.random()} />
+                  }
+                />
+                <Route
+                  path="/"
+                  element={
+                    <BasePage childView={<HomePage />} key={Math.random()} />
+                  }
+                />
+                <Route
+                  path="*"
+                  element={<Navigate replace to="/" key={Math.random()} />}
+                />
+              </Routes>
+              {wideScreen && <DrawerMenu />}
+            </main>
+          </div>
+        </Router>
+      )}
+    </Authenticator>
   );
 }
 
